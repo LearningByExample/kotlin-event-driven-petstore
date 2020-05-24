f8d3f32a8223caf5b2d4fa9e4be312f0b8d82f4d
Estefania Castro Vizoso <fany10@gmail.com>
2020-05-24 12:37:51 +0200

    init post request, working on #5

---
+++ pet-commands/src/main/kotlin/org/learning/by/example/petstore/petcommands/PetRoutes.kt
@@ -0,0 +1,22 @@
+package org.learning.by.example.petstore.petcommands
+
+import org.springframework.context.annotation.Bean
+import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.server.router
+
+@Component
+class PetRoutes {
+    @Bean
+    fun routes() = router {
+        accept(APPLICATION_JSON).nest {
+            contentType(APPLICATION_JSON).nest {
+                "/pet".nest {
+                    POST("/") {
+                        ok().build()
+                    }
+                }
+            }
+        }
+    }
+}
