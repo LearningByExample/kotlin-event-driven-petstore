d8736a42ab7572f9ef4af75cdc2dee84d9817665
Juan Medina <jamedina@gmail.com>
2020-05-25 09:23:36 +0100

    refactor test, working on #5

--- pet-commands/src/test/kotlin/org/learning/by/example/petstore/petcommands/PetRoutesTest.kt
+++ pet-commands/src/test/kotlin/org/learning/by/example/petstore/petcommands/PetRoutesTest.kt
@@ -1,6 +1,5 @@
 package org.learning.by.example.petstore.petcommands
 
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.DynamicTest.dynamicTest
 import org.junit.jupiter.api.TestFactory
@@ -13,79 +12,78 @@ import org.springframework.http.MediaType
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.reactive.server.WebTestClient
 import reactor.core.publisher.toMono
-import java.nio.charset.StandardCharsets
 
 @ExtendWith(SpringExtension::class)
 @SpringBootTest
 @AutoConfigureWebTestClient
 class PetRoutesTest {
-
     @Autowired
     private lateinit var webClient: WebTestClient
 
-    inner class TestCase(val name: String,
-                         val acceptType: MediaType,
-                         val contentType: MediaType,
-                         val statusExpected: HttpStatus,
-                         val responseExpected: String?)
-
-    @TestFactory
-    fun `Run multiple tests`(): Collection<DynamicTest> {
-        val body = """
+    companion object {
+        const val PET_URL = "/pet"
+        const val EXAMPLE_PET = """
             {
-              "name": "doggie",
+              "name": "dogie",
               "category": "string",
               "tags": [
                 "string1",
                 "string2",
               ],
             }
-        """.trimIndent()
-
-        val list = listOf(
-            TestCase(
-                "we should get 200 when posting a pet",
-                MediaType.APPLICATION_JSON,
-                MediaType.APPLICATION_JSON,
-                HttpStatus.OK,
-                "hello"
-            ),
-            TestCase(
-                "we should get 404 when posting not json",
-                MediaType.APPLICATION_JSON,
-                MediaType.TEXT_PLAIN,
-                HttpStatus.NOT_FOUND,
-                null
-            ),
-            TestCase(
-                "we should get 404 when accepting not json",
-                MediaType.TEXT_PLAIN,
-                MediaType.APPLICATION_JSON,
-                HttpStatus.NOT_FOUND,
-                null
-            )
-        )
-
-        return list.map {
-            dynamicTest(it.name) {
-                webClient.post()
-                    .uri("/pet")
-                    .accept(it.acceptType)
-                    .contentType(it.contentType)
-                    .body(body.toMono(), String::class.java)
-                    .exchange()
-                    .expectStatus().isEqualTo(it.statusExpected)
-                    .expectBody()
-                    .consumeWith { response ->
-                        if (response.status == HttpStatus.OK) {
-                            assertThat(response.responseBody).isNotNull()
-                            assertThat(response.responseBody?.toString(StandardCharsets.UTF_8)).isEqualTo(body)
-                        }
-                    }
-            }
-        }.toList()
+        """
     }
 
+    data class TestCase(val name: String, val parameters: Parameters, val expect: Expect) {
+        data class Parameters(val accept: MediaType, val contentType: MediaType)
+        data class Expect(val status: HttpStatus)
+    }
+
+    @TestFactory
+    fun `We should handle correctly accept and content type`() = listOf(
+        TestCase(
+            name = "we should be ok with correct types",
+            parameters = TestCase.Parameters(
+                accept = MediaType.APPLICATION_JSON,
+                contentType = MediaType.APPLICATION_JSON
+            ),
+            expect = TestCase.Expect(status = HttpStatus.OK)
+        ),
+        TestCase(
+            name = "we should not found with invalid accept",
+            parameters = TestCase.Parameters(
+                accept = MediaType.APPLICATION_JSON,
+                contentType = MediaType.TEXT_PLAIN
+            ),
+            expect = TestCase.Expect(status = HttpStatus.NOT_FOUND)
+        ),
+        TestCase(
+            name = "we should not found with invalid content",
+            parameters = TestCase.Parameters(
+                accept = MediaType.TEXT_PLAIN,
+                contentType = MediaType.APPLICATION_JSON
+            ),
+            expect = TestCase.Expect(status = HttpStatus.NOT_FOUND)
+        ),
+        TestCase(
+            name = "we should not found with invalid accept and content",
+            parameters = TestCase.Parameters(
+                accept = MediaType.TEXT_PLAIN,
+                contentType = MediaType.TEXT_PLAIN
+            ),
+            expect = TestCase.Expect(status = HttpStatus.NOT_FOUND)
+        )
+    ).map {
+        dynamicTest(it.name) {
+            webClient.post()
+                .uri(PET_URL)
+                .accept(it.parameters.accept)
+                .contentType(it.parameters.contentType)
+                .body(EXAMPLE_PET.toMono(), EXAMPLE_PET.javaClass)
+                .exchange()
+                .expectStatus().isEqualTo(it.expect.status)
+        }
+    }
 }
 
 
