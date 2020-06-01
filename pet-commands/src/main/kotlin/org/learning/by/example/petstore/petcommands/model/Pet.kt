cc81f11d9f4c6a5200fd82868984e8400dac95e0
Juan Medina <jamedina@gmail.com>
2020-06-01 07:27:27 +0100

    change @get for @field on constraints, working on #5

--- pet-commands/src/main/kotlin/org/learning/by/example/petstore/petcommands/model/Pet.kt
+++ pet-commands/src/main/kotlin/org/learning/by/example/petstore/petcommands/model/Pet.kt
@@ -1,13 +1,12 @@
 package org.learning.by.example.petstore.petcommands.model
 
-import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
 
 data class Pet(
-    @get:NotNull @get:Size(min = 3, max = 64)
+    @field:Size(min = 3, max = 64)
     val name: String,
 
-    @get:NotNull @get:Size(min = 3, max = 64)
+    @field:Size(min = 3, max = 64)
     val category: String
 )
