2e68625c450288bb588eb6d21fbbfdc94185e2a1
Juan Medina <jamedina@gmail.com>
2020-06-27 19:40:30 +0100

    created tests for CustomKafkaContainer , working on #7

--- command-producer-consumer/src/test/kotlin/org/learning/by/example/petstore/command/test/CustomKafkaContainer.kt
+++ command-producer-consumer/src/test/kotlin/org/learning/by/example/petstore/command/test/CustomKafkaContainer.kt
@@ -7,12 +7,16 @@ class CustomKafkaContainer : KafkaContainer() {
     companion object {
         private const val SCRIPT_PATH = "scripts"
         private const val SCRIPT_SEND_MESSAGE = "send_message.sh"
+        private const val SCRIPT_READ_MESSAGE = "read_message.sh"
         private const val SCRIPT_CREATE_TOPIC = "create_topic.sh"
         private const val CONTAINER_PATH = "/usr/helpers"
-        private const val CONTAINER_MESSAGE_COMMAND = "$CONTAINER_PATH/$SCRIPT_SEND_MESSAGE"
+        private const val CONTAINER_MESSAGE_SEND_COMMAND = "$CONTAINER_PATH/$SCRIPT_SEND_MESSAGE"
+        private const val CONTAINER_MESSAGE_READ_COMMAND = "$CONTAINER_PATH/$SCRIPT_READ_MESSAGE"
         private const val CONTAINER_TOPIC_COMMAND = "$CONTAINER_PATH/$SCRIPT_CREATE_TOPIC"
         private val CHMOD_CMD = arrayOf("chmod", "+xX")
     }
+
+    private val readOffsetPerTopic: HashMap<String, Int> = hashMapOf()
 
     init {
         withClasspathResourceMapping(SCRIPT_PATH, CONTAINER_PATH, BindMode.READ_ONLY)
@@ -20,7 +24,8 @@ class CustomKafkaContainer : KafkaContainer() {
 
     override fun start() {
         super.start()
-        execInContainer(*CHMOD_CMD, CONTAINER_MESSAGE_COMMAND)
+        execInContainer(*CHMOD_CMD, CONTAINER_MESSAGE_SEND_COMMAND)
+        execInContainer(*CHMOD_CMD, CONTAINER_MESSAGE_READ_COMMAND)
         execInContainer(*CHMOD_CMD, CONTAINER_TOPIC_COMMAND)
     }
 
@@ -28,5 +33,17 @@ class CustomKafkaContainer : KafkaContainer() {
         execInContainer(CONTAINER_TOPIC_COMMAND, topic).exitCode == 0
 
     fun sendMessage(topic: String, message: String): Boolean =
-        execInContainer(CONTAINER_MESSAGE_COMMAND, topic, message).exitCode == 0
+        execInContainer(CONTAINER_MESSAGE_SEND_COMMAND, topic, message).exitCode == 0
+
+    fun getMessage(topic: String): String {
+        val offset = readOffsetPerTopic.getOrDefault(topic, 0)
+        with(execInContainer(CONTAINER_MESSAGE_READ_COMMAND, topic, offset.toString())) {
+            return if (this.exitCode == 0) {
+                readOffsetPerTopic[topic] = offset + 1
+                this.stdout.trimIndent()
+            } else {
+                ""
+            }
+        }
+    }
 }
---
+++ command-producer-consumer/src/test/kotlin/org/learning/by/example/petstore/command/test/CustomKafkaContainerTest.kt
@@ -0,0 +1,29 @@
+package org.learning.by.example.petstore.command.test
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+
+@Testcontainers
+internal class CustomKafkaContainerTest {
+    companion object {
+        const val TEST_TOPIC = "test-topic"
+
+        @Container
+        private val KAFKA_CONTAINER = CustomKafkaContainer()
+    }
+
+    @Test
+    fun `we could send and read messages from a topic`() {
+        assertThat(KAFKA_CONTAINER.isRunning).isTrue()
+
+        assertThat(KAFKA_CONTAINER.createTopic(TEST_TOPIC)).isTrue()
+
+        assertThat(KAFKA_CONTAINER.sendMessage(TEST_TOPIC, "one")).isTrue()
+        assertThat(KAFKA_CONTAINER.sendMessage(TEST_TOPIC, "two")).isTrue()
+
+        assertThat(KAFKA_CONTAINER.getMessage(TEST_TOPIC)).isEqualTo("one")
+        assertThat(KAFKA_CONTAINER.getMessage(TEST_TOPIC)).isEqualTo("two")
+    }
+}
