84857cfa92fede0858dfd51c79e01be2bea0921c
Juan Medina <jamedina@gmail.com>
2020-06-28 06:36:36 +0100

    fix delete topic, working on #7

--- command-producer-consumer/src/test/resources/scripts/create_topic.sh
+++ command-producer-consumer/src/test/resources/scripts/create_topic.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/usr/bin/kafka-topics --delete --topic "$1"
+/usr/bin/kafka-topics --bootstrap-server localhost:9092 --delete --topic "$1"
 /usr/bin/kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic "$1"
