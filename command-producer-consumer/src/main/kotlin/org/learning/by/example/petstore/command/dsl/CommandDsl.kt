cb78989263163c79584d1aa02033668fc70b4a6c
Juan Medina <jamedina@gmail.com>
2020-07-01 10:44:15 +0100

    adding values to the command dsl, closes #12

--- command-producer-consumer/src/main/kotlin/org/learning/by/example/petstore/command/dsl/CommandDsl.kt
+++ command-producer-consumer/src/main/kotlin/org/learning/by/example/petstore/command/dsl/CommandDsl.kt
@@ -10,6 +10,10 @@ class CommandDsl(private val commandName: String) {
     infix fun String.value(value: Any) {
         payload[this] = value
     }
+
+    infix fun String.values(list: List<Any>) {
+        payload[this] = list
+    }
 }
 
 fun command(commandName: String, init: CommandDsl.() -> Unit) = with(CommandDsl(commandName)) {
--- command-producer-consumer/src/test/kotlin/org/learning/by/example/petstore/command/dsl/CommandDslTest.kt
+++ command-producer-consumer/src/test/kotlin/org/learning/by/example/petstore/command/dsl/CommandDslTest.kt
@@ -9,6 +9,7 @@ internal class CommandDslTest {
         private const val VALID_TIMESTAMP = "\\d{4}-(?:0[1-9]|1[0-2])-(?:0[1-9]|[1-2]\\d|3[0-1])T(?:[0-1]\\d|2[0-3])" +
             ":[0-5]\\d:[0-5]\\d(?:\\.\\d+|)(?:Z|(?:\\+|\\-)(?:\\d{2}):?(?:\\d{2}))"
     }
+
     @Test
     fun `we can create a command with attributes`() {
         val command = command("example command") {
@@ -16,6 +17,7 @@ internal class CommandDslTest {
             "attribute2" value 123
             "attribute3" value false
             "attribute4" value 125.5
+            "attribute5" values listOf("one", "two", "three")
         }
 
         Assertions.assertThat(command.commandName).isEqualTo("example command")
@@ -25,5 +27,6 @@ internal class CommandDslTest {
         Assertions.assertThat(command.get<Int>("attribute2")).isEqualTo(123)
         Assertions.assertThat(command.get<Boolean>("attribute3")).isEqualTo(false)
         Assertions.assertThat(command.get<Double>("attribute4")).isEqualTo(125.5)
+        Assertions.assertThat(command.getList<String>("attribute5")).isEqualTo(listOf("one", "two", "three"))
     }
 }
