f804e0eecf00c4399c54f48d619106c8679c438e
Estefania Castro Vizoso <estefania.castro.vizoso@gmail.com>
2020-07-12 10:58:11 +0200

    adding ktlint plugin, closes #17

--- pet-stream/src/main/kotlin/org/learning/by/example/petstore/petstream/PetStreamApplication.kt
+++ pet-stream/src/main/kotlin/org/learning/by/example/petstore/petstream/PetStreamApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class PetStreamApplication
 
 fun main(args: Array<String>) {
-	runApplication<PetStreamApplication>(*args)
+    runApplication<PetStreamApplication>(*args)
 }
--- pet-stream/src/test/kotlin/org/learning/by/example/petstore/petstream/PetStreamApplicationTests.kt
+++ pet-stream/src/test/kotlin/org/learning/by/example/petstore/petstream/PetStreamApplicationTests.kt
@@ -5,9 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class PetStreamApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
+    @Test
+    fun contextLoads() {
+    }
 }
