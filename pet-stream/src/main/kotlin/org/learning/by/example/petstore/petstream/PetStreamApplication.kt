94d2b3fff076d6647d90f68ab5999b9c0f131900
Estefania Castro Vizoso <estefania.castro.vizoso@gmail.com>
2020-07-12 10:43:53 +0200

    creating service, closes #15

---
+++ pet-stream/src/main/kotlin/org/learning/by/example/petstore/petstream/PetStreamApplication.kt
@@ -0,0 +1,11 @@
+package org.learning.by.example.petstore.petstream
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PetStreamApplication
+
+fun main(args: Array<String>) {
+	runApplication<PetStreamApplication>(*args)
+}
---
+++ pet-stream/src/test/kotlin/org/learning/by/example/petstore/petstream/PetStreamApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.learning.by.example.petstore.petstream
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PetStreamApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
