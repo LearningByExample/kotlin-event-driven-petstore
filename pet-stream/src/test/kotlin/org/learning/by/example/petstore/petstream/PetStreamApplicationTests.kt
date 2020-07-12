ff3e9e229dd189dae483de5ca68276fbe57b0776
Estefania Castro Vizoso <estefania.castro.vizoso@gmail.com>
2020-07-12 11:25:40 +0200

    add new test to verify any endpoint is not found, working on #18

--- pet-stream/src/test/kotlin/org/learning/by/example/petstore/petstream/PetStreamApplicationTests.kt
+++ pet-stream/src/test/kotlin/org/learning/by/example/petstore/petstream/PetStreamApplicationTests.kt
@@ -1,11 +1,21 @@
 package org.learning.by.example.petstore.petstream
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.reactive.server.WebTestClient
 
 @SpringBootTest
-class PetStreamApplicationTests {
+@AutoConfigureWebTestClient
+class PetStreamApplicationTests(@Autowired val webTestClient: WebTestClient) {
     @Test
-    fun contextLoads() {
+    fun `should return not found when any request`() {
+        webTestClient.get()
+            .uri("/")
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .expectStatus().isNotFound
     }
 }
