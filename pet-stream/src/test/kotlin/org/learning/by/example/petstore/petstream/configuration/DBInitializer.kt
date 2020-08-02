107bcd1b7804c3dcb363b40cc8e5ca7a7513085f
Estefania Castro Vizoso <estefania.castro.vizoso@gmail.com>
2020-08-02 10:18:42 +0200

    move DBInitializer.kt to test and move schema.sql to root path, closes #26

--- pet-stream/src/test/kotlin/org/learning/by/example/petstore/petstream/configuration/DBInitializer.kt
+++ pet-stream/src/test/kotlin/org/learning/by/example/petstore/petstream/configuration/DBInitializer.kt
@@ -2,7 +2,7 @@ package org.learning.by.example.petstore.petstream.configuration
 
 import io.r2dbc.spi.ConnectionFactory
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
-import org.springframework.core.io.ClassPathResource
+import org.springframework.core.io.FileSystemResource
 import org.springframework.data.r2dbc.connectionfactory.init.CompositeDatabasePopulator
 import org.springframework.data.r2dbc.connectionfactory.init.ConnectionFactoryInitializer
 import org.springframework.data.r2dbc.connectionfactory.init.ResourceDatabasePopulator
@@ -12,14 +12,14 @@ import org.springframework.stereotype.Service
 @ConditionalOnProperty(prefix = "db", name = ["initialize"], havingValue = "true")
 class DBInitializer(connectionFactory: ConnectionFactory) : ConnectionFactoryInitializer() {
     companion object {
-        const val SQL_SCHEMA_PATH = "sql/schema.sql"
+        const val SQL_SCHEMA_PATH = "../pet-sql/schema.sql"
     }
 
     init {
         this.setConnectionFactory(connectionFactory)
         this.setDatabasePopulator(
             CompositeDatabasePopulator().apply {
-                addPopulators(ResourceDatabasePopulator(ClassPathResource(SQL_SCHEMA_PATH)))
+                addPopulators(ResourceDatabasePopulator(FileSystemResource(SQL_SCHEMA_PATH)))
             }
         )
     }
