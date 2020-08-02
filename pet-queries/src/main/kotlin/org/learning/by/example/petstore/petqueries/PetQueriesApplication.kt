e5eadb85929ee6c3033662aec0076a185983ce0a
Estefania Castro Vizoso <estefania.castro.vizoso@gmail.com>
2020-08-02 10:36:07 +0200

    project created, closes #27

---
+++ pet-queries/src/main/kotlin/org/learning/by/example/petstore/petqueries/PetQueriesApplication.kt
@@ -0,0 +1,11 @@
+package org.learning.by.example.petstore.petqueries
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PetQueriesApplication
+
+fun main(args: Array<String>) {
+    runApplication<PetQueriesApplication>(*args)
+}
---
+++ pet-queries/src/test/kotlin/org/learning/by/example/petstore/petqueries/PetQueriesApplicationTests.kt
@@ -0,0 +1,11 @@
+package org.learning.by.example.petstore.petqueries
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PetQueriesApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
